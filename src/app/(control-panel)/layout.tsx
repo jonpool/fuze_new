715b2e8c1ad5fc4cdd5d024964b38b43dbc015d2
mustafa <mustafahlvc@gmail.com>
2024-10-19 18:50:52 +0300

    disable auth temporarily

--- src/app/(control-panel)/layout.tsx
+++ src/app/(control-panel)/layout.tsx
@@ -3,7 +3,7 @@ import AuthGuardRedirect from '@auth/AuthGuardRedirect';
 
 function Layout({ children }) {
 	return (
-		<AuthGuardRedirect auth={['admin']}>
+		<AuthGuardRedirect auth={null}>
 			<MainLayout>{children}</MainLayout>
 		</AuthGuardRedirect>
 	);
